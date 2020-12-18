41878b860bf8f443fbb3acfa9cc7b14b60f9d358
ANJUVIGIL <75435342+ANJUVIGIL@users.noreply.github.com>
2020-12-18 16:14:34 +0530

    Update urls.py

--- Finaldmez/dmezapp/urls.py
+++ Finaldmez/dmezapp/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('account/',views.account),
     path('consult/',views.consult),
     path('tracker/',views.tracker),
+    path('ayurvedhic/',views.ayurvedhic),
 
     path('cart/',views.cart, name='cart'),
     path('update_item/',views.updateItem, name='update_item'),
